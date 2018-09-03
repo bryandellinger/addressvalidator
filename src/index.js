69ce44b8777502e2de9e233593732ee091b826d5
Bryan Dellinger <dellingerfam5@gmail.com>
2018-09-03 18:14:18 -0400

    adding toastr

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
 import validate from 'jquery-validation';
+import * as toastr from 'toastr';
+import 'toastr/build/toastr.css';
 const loadGoogleMapsApi = require('load-google-maps-api');
 var geocoder = require('google-geocoder');
 var Promise = require("bluebird");
@@ -79,13 +81,19 @@ $(function() {
                 const formattedAddress = response[0].formatted_address;
                 $('#address').val(formattedAddress);
                 drawMap(response[0].location.lat, response[0].location.lng) //resolve callback(success)
-                if (response[0] && response[0].location_type === "APPROXIMATE"){
-                    alert('unable to find a street address for this location');
+                if (!response[0] || response[0].location_type === "GEOMETRIC_CENTER" || response[0].location_type === "RANGE_INTERPOLATED" ){
+                    toastr.warning('this address is an approximation,  please ensure it is accurate');
+                }
+                else if (response[0] && response[0].location_type === "ROOFTOP"){
+                    toastr.success('address validate');
+                }
+                else{
+                    toastr.error('unable to resolve address');
                 }
                 return false; // for demo
             }).catch(function(error){
                 $('.map').empty();
-                alert('unable to resolve address');
+                toastr.error('unable to resolve address');
                 console.log(error) //reject callback(failure)
                 return false; // for demo
             })          
